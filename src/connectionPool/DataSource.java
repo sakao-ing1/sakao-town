a44e785944f9811547f05bc6bf36037596329c2c
sakao-town <azizbelkadhi.ab@gmail.com>
2020-02-27 17:56:35 +0100

    Aziz push

--- src/connectionPool/DataSource.java
+++ src/connectionPool/DataSource.java
@@ -1,5 +1,18 @@
 package connectionPool;
 
+import java.sql.SQLException;
+
 public class DataSource {
+
+	private static JDBCConnectionPool instance;
+
+	
+	
+	public static JDBCConnectionPool getInstance() throws SQLException {
+		if (instance == null)
+			instance = new JDBCConnectionPool();
+
+		return instance;
+	}
 
 }
