ccb38a21526afb8955ed93b58cfbc4c9e69f46ec
AzizBELKADHI <azizbelkadhi.ab@gmail.com>
2020-02-28 14:06:40 +0100

    Classes : DataSource et JDBCConnectionPool

--- sakao-connectionpool/connectionPool/DataSource.java
+++ sakao-connectionpool/connectionPool/DataSource.java
@@ -1,18 +1,25 @@
 package connectionPool;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DataSource {
 
-	private static JDBCConnectionPool instance;
+	private static JDBCConnectionPool instance = new JDBCConnectionPool();
 
-	
-	
-	public static JDBCConnectionPool getInstance() throws SQLException {
-		if (instance == null)
-			instance = new JDBCConnectionPool();
+	// methode statique qui renvoie une connexion de l'attribut
+	public Connection getConnection(String url) {
+		return instance.getConnection(url);
+	}
 
-		return instance;
+	// methode statique qui remet une connexion dans l'attribut
+	public void reset(Connection con) {
+		instance.setConnection(con);
+	}
+
+	// methode statique qui clot l'attribut.
+	public static void closeInstance() {
+		instance.closeAllConnection();
 	}
 
 }
--- sakao-connectionpool/connectionPool/JDBCConnectionPool.java
+++ sakao-connectionpool/connectionPool/JDBCConnectionPool.java
@@ -12,10 +12,12 @@ public class JDBCConnectionPool {
 
 	private Collection<Connection> listConnection = new ArrayList<Connection>();
 
-	public void remplir(Connection con) throws ClassNotFoundException {
+	// methode qui remplit l'attribut avec un certain nombre d'instances de la
+	// classe Connection
+	public void fill() throws ClassNotFoundException {
 		try {
 			Class.forName("");
-			con = DriverManager.getConnection("", "postgres", "system");
+			Connection con = DriverManager.getConnection("", "postgres", "system");
 			listConnection.add(con);
 			System.out.println("connexion établie!!!!");
 
@@ -24,13 +26,39 @@ public class JDBCConnectionPool {
 		}
 	}
 
-	
-	 /* public Connection getConnection(String url) { 
-		  for(Connection con : con)
-	  
-	 
-	  }*/
-	  
-	 
+	// methode qui renvoie un objet Connection pris dans l'attribut
+	public Connection getConnection(String url) {
+		for (Connection con : listConnection) {
+			String url2;
+			try {
+				url2 = con.getMetaData().getURL();
+				if (url2.equals(url)) {
+					return con;
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	// methode avec un parametre instance de la classe Connection qui remet cet
+	// objet dans l'attribut
+	public void setConnection(Connection connection) {
+		this.listConnection.add(connection);
+	}
+
+	// methode qui ferme toutes les connexions de l'attribut
+	public void closeAllConnection() {
+		for (Connection connection : listConnection) {
+			try {
+				connection.close();
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
 
 }
