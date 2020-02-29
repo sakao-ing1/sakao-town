7182410f2c91b645a43419fc02205711c8379ec0
AzizBELKADHI <azizbelkadhi.ab@gmail.comr>
2020-02-29 13:50:13 +0100

    Interaction avec la base (CRUD) + creation de la table personne pour faire le test

--- src/sakao_connection_pool/JDBCConnectionPool.java
+++ src/sakao_connection_pool/JDBCConnectionPool.java
@@ -1,11 +1,14 @@
 package sakao_connection_pool;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,7 +24,8 @@ public class JDBCConnectionPool {
 		try {
 
 			Class.forName(ConnectionFileReader.getDriver());
-			Connection con = DriverManager.getConnection(ConnectionFileReader.getUrl(), ConnectionFileReader.getLogin(), ConnectionFileReader.getPassword());
+			Connection con = DriverManager.getConnection(ConnectionFileReader.getUrl(), ConnectionFileReader.getLogin(),
+					ConnectionFileReader.getPassword());
 			listConnection.add(con);
 			System.out.println("connexion établie!!!!");
 
@@ -64,5 +68,67 @@ public class JDBCConnectionPool {
 		}
 
 	}
+
+	// Partie CRUD
+
+	// Requete SELECT
+	public List<Personne> showPersonne() {
+		List<Personne> retour = new ArrayList<Personne>();
+		try {
+			PreparedStatement pt = this.getConnection(ConnectionFileReader.getUrl())
+					.prepareStatement("select * from personne");
+			ResultSet rs = pt.executeQuery();
+			while (rs.next()) {
+				int id = rs.getInt(1);
+				String name = rs.getString(2);
+				int age = rs.getInt(3);
+				retour.add(new Personne(id, name, age));
+			}
+		} catch (SQLException ex) {
+			System.out.println("erreur " + ex.getMessage());
+		}
+		return retour;
+	}
+
+	// Requete DELETE
+	  public void deletePersonneByName(String name) {
+	        try {
+	            PreparedStatement pt = this.getConnection(ConnectionFileReader.getUrl()).prepareStatement("delete from personne where name like ?");
+	            pt.setString(1, name);
+	            pt.execute();
+	        } catch (SQLException ex) {
+	            System.out.println("erreur " + ex.getMessage());
+	        }
+
+	    }
+
+	// Requete CREATE
+
+    public void addPersonne(Personne p) {
+        try {
+
+            String req = "insert into personne(name,age) values (?,?)";
+            PreparedStatement pstm = this.getConnection(ConnectionFileReader.getUrl()).prepareStatement(req);
+            pstm.setString(1, p.getName());
+            pstm.setInt(2, p.getAge());
+            pstm.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+	// Requete UPDATE
+    
+    public void updatePersonne(int id, String name, int age) {
+        try {
+            PreparedStatement pstm = this.getConnection(ConnectionFileReader.getUrl()).prepareStatement(" UPDATE personne SET name= ? ,age= ? WHERE id=? ");
+            pstm.setString(1, name);
+            pstm.setInt(2, age);
+            pstm.setInt(3, id);
+            pstm.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("erreur " + ex.getMessage());
+        }
+    }
 
 }
--- src/sakao_connection_pool/testMain.java
+++ src/sakao_connection_pool/testMain.java
@@ -6,6 +6,11 @@ public class testMain {
 
 	public static void main(String[] args) throws ClassNotFoundException {
 		DataSource ds = new DataSource();
+		ds.showPersonne();
+		ds.addPersonne("kumanan", 66);
+		ds.deletePersonne("aziz");
+		ds.updatePersonne(2, "alain", 60);
+		
 	}
 
 }
