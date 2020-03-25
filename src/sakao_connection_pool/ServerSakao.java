9bad0793045b0b3362fe7a2dd42181877efe24b8
Alain-SARKISIAN <alain.sarkisian@u-pec.fr>
2020-03-25 15:54:27 +0100

    MAJ arborescence

---
+++ src/sakao_connection_pool/ServerSakao.java
@@ -0,0 +1,10 @@
+package sakao_connection_pool;
+
+public class ServerSakao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/sakao_server/ServerSakao.java
+++ src/sakao_server/ServerSakao.java
@@ -7,12 +7,14 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
+import org.json.JSONObject;
+
 public class ServerSakao {
 	private ServerSocket serverSocket;
 	private Socket clientSocket;
 	private OutputStreamWriter out;
 	private BufferedReader in;
-	/////private JSONObject json = new JSONObject();
+	private JSONObject json = new JSONObject();
 	
 	
 	public void start(int port) throws IOException {
